5608534094a8e3f9205d70bdc85a6927bb735779
Daniel 'Mevoroth' Quach <darkskana@gmail.com>
2021-06-19 18:03:40 +0200

    Update project Fix descriptor layout

--- ReShieldProject/WinMain.cpp
+++ ReShieldProject/WinMain.cpp
@@ -113,7 +113,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	LPSTR lpCmdLine,
 	int nCmdShow)
 {
-	Eternal::DebugTools::WaitForDebugger(false);
+	Eternal::DebugTools::WaitForDebugger();
 
 	using namespace Eternal::Graphics;
 	RenderSettings Settings(1600, 900, DeviceType::VULKAN);
@@ -177,7 +177,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 			//////////////////////////////////////////////////////////////////////////
 			// GPU
-			vk::Image Texture;
+			vk::Image& Texture = *new vk::Image;
 			vk::DeviceMemory TextureMemory;
 
 			vk::ImageCreateInfo TextureInfo(
@@ -269,6 +269,8 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 			TextureData.Release();
 
+			OutTexture = reinterpret_cast<Eternal::Graphics::Texture*>(&Texture);
+
 			return true;
 		}
 
@@ -301,7 +303,9 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	);
 
 	vector<RootSignatureParameter> Parameters = {
-		{ RootSignatureParameterType::ROOT_SIGNATURE_PARAMETER_DYNAMIC_BUFFER, RootSignatureAccess::ROOT_SIGNATURE_ACCESS_PS, 0 }
+		{ RootSignatureParameterType::ROOT_SIGNATURE_PARAMETER_DYNAMIC_BUFFER, RootSignatureAccess::ROOT_SIGNATURE_ACCESS_PS, 0 },
+		{ RootSignatureParameterType::ROOT_SIGNATURE_PARAMETER_TEXTURE, RootSignatureAccess::ROOT_SIGNATURE_ACCESS_PS, 1 },
+		{ RootSignatureParameterType::ROOT_SIGNATURE_PARAMETER_SAMPLER, RootSignatureAccess::ROOT_SIGNATURE_ACCESS_PS, 2 }
 	};
 
 	RootSignature* DefaultRootSignature = CreateRootSignature(*Context, &Parameters, 1, RootSignatureAccess::ROOT_SIGNATURE_ACCESS_PS);
@@ -417,15 +421,24 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	vk::DeviceMemory BufferMemory;
 	Vulkan::VerifySuccess(DeviceObj.allocateMemory(&MemoryInfo, nullptr, &BufferMemory));
 
-	vk::DescriptorPoolSize PoolSize(
+	std::array<vk::DescriptorPoolSize, 3> PoolSizes;
+	PoolSizes[0] = vk::DescriptorPoolSize(
 		vk::DescriptorType::eUniformBufferDynamic,
+		16
+	);
+	PoolSizes[1] = vk::DescriptorPoolSize(
+		vk::DescriptorType::eSampledImage,
+		16
+	);
+	PoolSizes[2] = vk::DescriptorPoolSize(
+		vk::DescriptorType::eSampler,
 		16
 	);
 
 	vk::DescriptorPoolCreateInfo DescriptorPoolInfo(
 		vk::DescriptorPoolCreateFlagBits(),
 		2,
-		1, &PoolSize
+		PoolSizes.size(), PoolSizes.data()
 	);
 	vk::DescriptorPool DescPool;
 	Vulkan::VerifySuccess(DeviceObj.createDescriptorPool(&DescriptorPoolInfo, nullptr, &DescPool));
@@ -441,6 +454,21 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		&DescriptorSetInfo,
 		&FrameConstantsDescriptorSet
 	);
+
+	vk::ImageView NoiseTextureView;
+
+	vk::Sampler NoiseTextureSampler;
+	vk::SamplerCreateInfo NoiseTextureSamplerCreateInfo(
+		vk::SamplerCreateFlagBits(),
+		vk::Filter::eLinear,
+		vk::Filter::eLinear,
+		vk::SamplerMipmapMode::eLinear
+	);
+	Vulkan::VerifySuccess(DeviceObj.createSampler(
+		&NoiseTextureSamplerCreateInfo,
+		nullptr,
+		&NoiseTextureSampler
+	));
 
 	TextureFactoryRequest Request("Noise", ".\\noise.tga");
 	TexFactory.CreateRequest(Request);
@@ -511,23 +539,60 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		if (i == 0)
 			DeviceObj.bindBufferMemory(PerFrameConstantBuffer, BufferMemory, 0ull);
 
-		vk::DeviceSize Range = sizeof(PerFrame);
+		std::array<vk::WriteDescriptorSet, 2> NewDescriptorSets;
 
+		vk::DeviceSize Range = sizeof(PerFrame);
 		vk::DescriptorBufferInfo BufferDescriptorInfo(
 			PerFrameConstantBuffer,
 			0, Range
 		);
-
-		vk::WriteDescriptorSet NewDescriptorSet(
+		NewDescriptorSets[0] = vk::WriteDescriptorSet(
 			FrameConstantsDescriptorSet,
 			0, 0, 1, vk::DescriptorType::eUniformBufferDynamic,
 			nullptr, &BufferDescriptorInfo, nullptr
 		);
 
+		vk::Image* NoiseTexture = reinterpret_cast<vk::Image*>(TexFactory.GetTexture("Noise"));
+		if (NoiseTexture && !NoiseTextureView)
+		{
+			vk::ImageViewCreateInfo NoiseImageViewInformation(
+				vk::ImageViewCreateFlagBits(),
+				*NoiseTexture,
+				vk::ImageViewType::e2D,
+				vk::Format::eR8G8B8A8Unorm,
+				vk::ComponentMapping(),
+				vk::ImageSubresourceRange(
+					vk::ImageAspectFlagBits::eColor,
+					0, 1, 0, 1
+				)
+			);
+			Vulkan::VerifySuccess(DeviceObj.createImageView(&NoiseImageViewInformation, nullptr, &NoiseTextureView));
+		}
+
+		vk::DescriptorImageInfo ImageDescriptorInfo(
+			vk::Sampler(),
+			NoiseTextureView,
+			vk::ImageLayout::eShaderReadOnlyOptimal
+		);
+
+		NewDescriptorSets[1] = vk::WriteDescriptorSet(
+			FrameConstantsDescriptorSet,
+			1, 0, 1, vk::DescriptorType::eSampledImage,
+			&ImageDescriptorInfo, nullptr, nullptr
+		);
+
 		DeviceObj.updateDescriptorSets(
-			1, &NewDescriptorSet,
+			NewDescriptorSets.size(), NewDescriptorSets.data(),
 			0, nullptr
 		);
+
+		//for (int32_t NewDescriptorSetIndex = 0; NewDescriptorSetIndex < NewDescriptorSets.size(); ++NewDescriptorSetIndex)
+		//{
+		//	DeviceObj.updateDescriptorSets(
+		//		1, &NewDescriptorSets[NewDescriptorSetIndex],
+		//		0, nullptr
+		//	);
+		//}
 
 		VulkanCommandBuffer.pipelineBarrier(
 			vk::PipelineStageFlagBits::eAllGraphics,
@@ -546,8 +611,10 @@ int WINAPI WinMain(HINSTANCE hInstance,
 			vk::PipelineBindPoint::eGraphics,
 			((VulkanRootSignature*)DefaultRootSignature)->GetPipelineLayout(),
 			0,
-			1, &FrameConstantsDescriptorSet,
-			1, &DynamicOffset
+			0, nullptr,
+			//1, FrameConstantsDescriptorSet,
+			//1, &DynamicOffset
+			0, nullptr
 		);
 		VulkanCommandBuffer.draw(6, 1, 0, 0);
 		VulkanCommandBuffer.endRenderPass();
@@ -610,170 +677,6 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 	DestroyGraphicsContext(Context);
 	
-
-//
-//	//new D3D12ShaderFactory();
-//	//D3D12Device::Initialize();
-//	//D3D12Device DeviceObj(0);
-//
-//	VulkanDevice DeviceObj(WindowObj);
-//	CommandQueue& DirectCommandQueue = *CreateCommandQueue(DeviceObj, 2);
-//	SwapChain& SwapChainObj = *CreateSwapChain(DeviceObj, WindowObj, DirectCommandQueue);
-//
-//	//Shader* VS = D3D12ShaderFactory::Get()->CreateVertexShader("PostProcess", "postprocess.vs.hlsl");
-//	//Shader* PS = D3D12ShaderFactory::Get()->CreatePixelShader("DefaultPostProcess", "defaultpostprocess.ps.hlsl");
-//	
-//	VulkanShader VS(DeviceObj, "PostProcessVS", "postprocess.vs.hlsl", VS);
-//	VulkanShader PS(DeviceObj, "DefaultPostProcessPS", "defaultpostprocess.ps.hlsl", PS);
-//	
-//	VulkanHeap HeapTexture(DeviceObj, 1024 * 4, 2, true, false, false, false);
-//	VulkanHeap HeapStaging(DeviceObj, 1024 * 4, 2, false, true, true, false);
-//
-//	VulkanResource BufferObj(DeviceObj, HeapTexture, 1024 * 4, (ResourceType)(BUFFER_STRUCTURED_READ | BUFFER_TRANSFER_WRITE));
-//	VulkanResource StagingObj(DeviceObj, HeapStaging, 1024 * 4, BUFFER_TRANSFER_READ);
-//
-//	//TextureObj.CreateView(DeviceObj, )
-//	RootSignature& RootSignatureObj = *CreateRootSignature(DeviceObj);
-//	VulkanDescriptorHeap DescriptorHeapObj(DeviceObj, STRUCTURED_BUFFER, 2, (RootSignatureAccess)(ROOT_SIGNATURE_VS | ROOT_SIGNATURE_PS));
-//	DescriptorHeap* DescriptorHeaps[]
-//	{
-//		&DescriptorHeapObj
-//	};
-//	RootSignature& RootSignatureWithSB = *CreateRootSignature(DeviceObj, nullptr, 0u, DescriptorHeaps, 1, (RootSignatureAccess)(ROOT_SIGNATURE_VS | ROOT_SIGNATURE_PS));
-//
-//	View* ViewObj = BufferObj.CreateView(DeviceObj, DescriptorHeapObj, BGRA8888);
-//
-//	//InputLayout::VertexDataType Vertices[] = {
-//	//	InputLayout::POSITION_T
-//	//};
-//	//D3D12InputLayout InputLayoutObj(Vertices, ETERNAL_ARRAYSIZE(Vertices));
-//	//DepthTest DepthTestObj;
-//	//StencilTest StencilTestObj;
-//	//BlendState BlendStates[] = {
-//	//	BlendState(BlendState::ONE, BlendState::ZERO, BlendState::OP_ADD, BlendState::ONE, BlendState::ZERO, BlendState::OP_ADD),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState()
-//	//};
-//	//D3D12InputLayout InputLayoutObj(Vertices, ETERNAL_ARRAYSIZE(Vertices));
-//	//DepthTest DepthTestObj;
-//	//StencilTest StencilTestObj;
-//	//BlendState BlendStates[] = {
-//	//	BlendState(BlendState::ONE, BlendState::ZERO, BlendState::OP_ADD, BlendState::ONE, BlendState::ZERO, BlendState::OP_ADD),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState(),
-//	//	BlendState()
-//	//};
-//
-//	//D3D12State StateObj(DeviceObj, InputLayoutObj, VS, PS, DepthTestObj, StencilTestObj, BlendStates, /*SwapChainObj.GetBackBuffer(0),*/ 1, nullptr, 0);
-//
-//	//D3D12CommandList CommandLists(DeviceObj, DirectCommandQueue, StateObj);
-//	VulkanCommandList CommandLists[] = {
-//		VulkanCommandList(DeviceObj, *DirectCommandQueue.GetCommandAllocator(0)),
-//		VulkanCommandList(DeviceObj, *DirectCommandQueue.GetCommandAllocator(1))
-//	};
-//
-//	//D3D12Pipeline PipelineObj(DeviceObj, RootSignatureObj, InputLayoutObj, VS, PS, DepthTestObj, StencilTestObj, BlendStates, /*SwapChainObj.GetBackBuffer(0),*/ 1, nullptr, 0);
-//
-//	Fence& FenceObj = *CreateFence(DeviceObj, 2);
-//	VulkanPipeline PipelineObj(DeviceObj, RootSignatureWithSB, SwapChainObj.GetMainRenderPass(), VS, PS, ViewportObj);
-//
-//	VkDescriptorSet DescriptorSets[] =
-//	{
-//		static_cast<VulkanDescriptorHeap&>(DescriptorHeapObj).Pop(),
-//		static_cast<VulkanDescriptorHeap&>(DescriptorHeapObj).Pop()
-//	};
-//
-	for (;;)
-	{
-//		FenceObj.Wait(DeviceObj);
-//		FenceObj.Reset(DeviceObj);
-//		uint32_t CurrentFrame = SwapChainObj.AcquireFrame(DeviceObj, FenceObj);
-//		DirectCommandQueue.Reset(CurrentFrame);
-//		//CommandLists.Begin(*DirectCommandQueue.GetCommandAllocator(CurrentFrame), PipelineObj);
-//		//CommandLists.SetViewport(ViewportObj);
-//		//CommandLists.SetScissorRectangle(ViewportObj);
-//		//CommandLists.GetD3D12GraphicsCommandList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-//		//static_cast<D3D12RenderTarget&>(SwapChainObj.GetBackBuffer(CurrentFrame)).Transition(CommandLists, RENDERTARGET);
-//		//CommandLists.BindRenderTarget(0, SwapChainObj.GetBackBufferView(CurrentFrame));
-//		//CommandLists.DrawPrimitive(6);
-//		//static_cast<D3D12RenderTarget&>(SwapChainObj.GetBackBuffer(CurrentFrame)).Transition(CommandLists, PRESENT);
-//		//CommandLists.End();
-//
-//		CommandLists[CurrentFrame].Begin(*DirectCommandQueue.GetCommandAllocator(CurrentFrame), PipelineObj);
-//		
-//		void* Data = StagingObj.Map(DeviceObj);
-//		for (int i = 0; i < 1024; ++i)
-//		{
-//			((int*)Data)[i] = 0xFF | (i % 0xFF) << 8 | ((i >> 8) * 0xFF) << 24;
-//		}
-//		StagingObj.Unmap(DeviceObj);
-//
-//		ResourceTransition CopyBufferTransitions[] = {
-//			ResourceTransition(&StagingObj, TRANSITION_UNDEFINED, TRANSITION_TRANSFER_READ),
-//			ResourceTransition(&BufferObj, TRANSITION_UNDEFINED, TRANSITION_TRANSFER_WRITE)
-//		};
-//		CommandLists[CurrentFrame].Transition(CopyBufferTransitions, ETERNAL_ARRAYSIZE(CopyBufferTransitions), nullptr, 0);
-//		CommandLists[CurrentFrame].CopyBuffer(StagingObj, BufferObj);
-//
-//		ResourceTransition UseBufferTransition(&BufferObj, TRANSITION_TRANSFER_WRITE, TRANSITION_SHADER_READ);
-//		CommandLists[CurrentFrame].Transition(&UseBufferTransition, 1, nullptr, 0);
-//
-//		CommandLists[CurrentFrame].BeginRenderPass(SwapChainObj.GetMainRenderPass(), SwapChainObj.GetBackBufferView(CurrentFrame).GetAsRenderTarget(), ViewportObj);
-//
-//#pragma region DescriptorSetStuffTest
-//		VkDescriptorBufferInfo BufferDescription;
-//		BufferDescription.buffer = static_cast<VulkanResource&>(BufferObj).GetBuffer();
-//		BufferDescription.offset = 0;
-//		BufferDescription.range = 1024 * 4;
-//
-//		VkWriteDescriptorSet WriteDescriptorSet;
-//		WriteDescriptorSet.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-//		WriteDescriptorSet.pNext = nullptr;
-//		WriteDescriptorSet.dstSet = DescriptorSets[CurrentFrame];
-//		WriteDescriptorSet.dstBinding = 0;
-//		WriteDescriptorSet.dstArrayElement = 0;
-//		WriteDescriptorSet.descriptorCount = 1;
-//		WriteDescriptorSet.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER;
-//		WriteDescriptorSet.pImageInfo = nullptr;
-//		WriteDescriptorSet.pBufferInfo = &BufferDescription;
-//		WriteDescriptorSet.pTexelBufferView = &static_cast<VulkanView*>(ViewObj)->GetBufferView();
-//
-//		vkUpdateDescriptorSets(static_cast<VulkanDevice&>(DeviceObj).GetVulkanDevice(), 1, &WriteDescriptorSet, 0, nullptr);
-//
-//		vkCmdBindDescriptorSets(CommandLists[CurrentFrame].GetVulkanCommandList(), VK_PIPELINE_BIND_POINT_GRAPHICS, static_cast<VulkanRootSignature&>(RootSignatureWithSB).GetPipelineLayout(), 0, 1, &DescriptorSets[CurrentFrame], 0, nullptr);
-//#pragma endregion DescriptorSetStuffTest
-//
-//		CommandLists[CurrentFrame].DrawPrimitive(6);
-//		CommandLists[CurrentFrame].EndRenderPass();
-//		CommandLists[CurrentFrame].End();
-//
-//		FenceObj.Reset(DeviceObj);
-//		CommandList* VulkanCommandLists[] = {
-//			&CommandLists[CurrentFrame]
-//			//&CommandLists
-//		};
-//		DirectCommandQueue.Submit(CurrentFrame, VulkanCommandLists, ETERNAL_ARRAYSIZE(VulkanCommandLists), FenceObj, SwapChainObj);
-//		FenceObj.Signal(DirectCommandQueue);
-//		SwapChainObj.Present(DeviceObj, DirectCommandQueue, CurrentFrame);
-
-		MSG Message = { 0 };
-		if (PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&Message);
-			DispatchMessage(&Message);
-		}
-		i++;
-	}
-
 	//CoreState::CoreStateSettings Settings;
 	//Settings.ShaderIncludePath = "..\\eternal-engine-shaders\\Shaders\\";
 	//Settings.FBXPath = "..\\assets\\fbx\\";
